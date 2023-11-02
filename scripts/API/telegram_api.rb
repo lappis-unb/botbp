084feaa711ddbed8129ea927f97ca305ab1b3b29
Giovanni Alvissus <giovanni.acg@gmail.com>
2023-11-02 14:57:30 -0300

    add: adicionando validacao de admins do bot

--- scripts/API/telegram_api.rb
+++ scripts/API/telegram_api.rb
@@ -1,12 +1,13 @@
 require 'json'
 require 'telegram/bot'
 require 'dotenv/load'
+require_relative '../../data/data_manager'
 
 module BotBP
   class TelegramAPI
     def initialize
       @token = ENV['TELEGRAM_TOKEN']
-
+      @users = BotBP::DataUsers.new
     end
 
     def run_bot
@@ -14,7 +15,15 @@ module BotBP
         bot.listen do |message|
           case message.text
           when '/start'
-            puts message.from.id
+            response = verify_user(message.chat.id)
+            case response
+            when 'admin'
+              bot.api.send_message(chat_id: message.chat.id, text: "id: #{message.chat.id} | admin")
+            when 'client'
+              bot.api.send_message(chat_id: message.chat.id, text: "id: #{message.chat.id} | client")
+            when 'none'
+              bot.api.send_message(chat_id: message.chat.id, text: "id: #{message.chat.id} | none")
+            end
           when '/stop'
             bot.api.send_message(chat_id: message.chat.id, text: "Até logo!")
           end
@@ -23,7 +32,13 @@ module BotBP
     end
 
     def verify_user (user_id)
+      users = @users.read('admin') # Lê a lista de usuários "admin"
+      unless users.nil?
+        admin = users.find { |admin| admin['telegram_user_id'] == user_id }
+        return 'admin' if admin
+      end
 
+      'none'
     end
   end
 end
