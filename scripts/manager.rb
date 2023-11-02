4dc8f5683305ac9f0e3470d80227484aebc91812
Giovanni Alvissus <giovanni.acg@gmail.com>
2023-11-02 14:58:18 -0300

    add: atualizando manager para funcionar como classe

--- scripts/manager.rb
+++ scripts/manager.rb
@@ -3,27 +3,39 @@ require_relative 'API/telegram_api'
 require_relative 'server/web_hook_app'
 require_relative 'server/post_to_web_hook'
 
-webhook = BotBP::WebHookApp
+module BotBP
+  class Manager
 
-thread_1 = Thread.new do
-  webhook.run!
-end
+    def start_software
 
-url = 'http://localhost:4567/gitlab-webhook'
-request_body = { key1: 'value1', key2: 'value2' }.to_json
+      thread_bot = Thread.new do
+        bot_telegram = BotBP::TelegramAPI.new
+        bot_telegram.run_bot
+      end
 
-post_html = BotBP::PostToWebHook.new
+      thread_webhook = Thread.new do
+        webhook = BotBP::WebHookApp
+        webhook.run!
+      end
 
-thread_2 = Thread.new do
-  sleep(3)
-  10.times do
-    post_html.post_to_webhook(url, request_body)
-    sleep(3)
+      thread_test = Thread.new do
+        url = 'http://localhost:4567/gitlab-webhook'
+        request_body = { key1: 'value1', key2: 'value2' }.to_json
+        post_html = BotBP::PostToWebHook.new
+        sleep(6)
+        10.times do
+          post_html.post_to_webhook(url, request_body)
+          sleep(3)
+        end
+      end
+
+      thread_bot.join
+      thread_webhook.join
+      thread_test.join
+    end
   end
 end
 
-thread_1.join
-thread_2.join
-
-
+app = BotBP::Manager.new
+app.start_software
 
