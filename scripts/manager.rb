b8c3167dc99f26a220413c151ef6593fa991b018
Giovanni Alvissus <giovanni.acg@gmail.com>
2023-11-05 20:55:11 -0300

    refatored: organizando manager.rb

--- scripts/manager.rb
+++ scripts/manager.rb
@@ -1,23 +1,38 @@
-require_relative 'API/gitlab_api'
-require_relative 'API/telegram_api'
+require_relative 'api/gitlab_api'
+require_relative 'api/telegram_api'
 require_relative 'server/web_hook_app'
 require_relative 'server/post_to_web_hook'
+require_relative 'log/log'
 
 module BotBP
   class Manager
-
     def start_software
+      start_bot
+      start_webhook
+      start_test
+    end
 
+    private
+
+    def start_bot
       thread_bot = Thread.new do
         bot_telegram = BotBP::TelegramAPI.new
         bot_telegram.run_bot
       end
 
+      thread_bot.join
+    end
+
+    def start_webhook
       thread_webhook = Thread.new do
         webhook = BotBP::WebHookApp
         webhook.run!
       end
 
+      thread_webhook.join
+    end
+
+    def start_test
       thread_test = Thread.new do
         url = 'http://localhost:4567/gitlab-webhook'
         request_body = { key1: 'value1', key2: 'value2' }.to_json
@@ -29,8 +44,6 @@ module BotBP
         end
       end
 
-      thread_bot.join
-      thread_webhook.join
       thread_test.join
     end
   end
