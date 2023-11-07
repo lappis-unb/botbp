42e349a5e94ad458ab9eeebd0e9af8744476285f
Giovanni Alvissus <giovanni.acg@gmail.com>
2023-11-06 22:13:54 -0300

    fix: corrigindo maneira de lidar com threads

--- app/manager.rb
+++ app/manager.rb
@@ -4,34 +4,44 @@ require_relative 'server/web_hook_app'
 require_relative 'server/post_to_web_hook'
 require_relative 'log/log'
 require_relative 'data/report_manager'
+require 'thread'
 
 module BotBP
   class Manager
+    @@bot_telegram = nil
+    @@threads = []
+
+    def self.bot_telegram
+      @@bot_telegram
+    end
+
     def start_software
-      start_report
+      # Inicie as threads
       start_bot
       start_webhook
       start_test
+      start_report
+
+      # Aguarde todas as threads concluírem
+      @@threads.each(&:join)
     end
 
     private
 
     def start_bot
       thread_bot = Thread.new do
-        bot_telegram = BotBP::TelegramAPI.new
-        bot_telegram.run_bot
+        @@bot_telegram = BotBP::TelegramAPI.new
+        @@bot_telegram.run_bot
       end
-
-      thread_bot.join
+      @@threads << thread_bot
     end
 
     def start_webhook
       thread_webhook = Thread.new do
-        webhook = BotBP::WebHookApp
-        webhook.run!
+        @webhook = BotBP::WebHookApp
+        @webhook.run!
       end
-
-      thread_webhook.join
+      @@threads << thread_webhook
     end
 
     def start_test
@@ -41,12 +51,12 @@ module BotBP
         post_html = BotBP::PostToWebHook.new
         sleep(6)
         10.times do
+          puts "estou enviando a requisicao"
           post_html.post_to_webhook(url, request_body)
           sleep(3)
         end
       end
-
-      thread_test.join
+      @@threads << thread_test
     end
 
     def start_report
@@ -54,12 +64,10 @@ module BotBP
         report_manager = BotBP::ReportManager.new
         report_manager.schedule_reports
       end
-
-      thread_report.join
+      @@threads << thread_report
     end
   end
 end
 
 app = BotBP::Manager.new
 app.start_software
-
