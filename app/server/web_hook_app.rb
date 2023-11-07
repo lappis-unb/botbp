106fa32d77d7e765deeacdc102388033e7142a4c
Giovanni Alvissus <giovanni.acg@gmail.com>
2023-11-06 22:14:43 -0300

    feat: chamando bot ao receber atualizacoes

--- app/server/web_hook_app.rb
+++ app/server/web_hook_app.rb
@@ -9,14 +9,11 @@ module BotBP
     post '/gitlab-webhook' do
       payload = JSON.parse(request.body.read)
 
-      # Processar os dados recebidos do webhook do GitLab aqui
-      # Você pode acessar os dados do GitLab através do objeto 'payload'
-
-      puts "Recebido um evento do GitLab:"
-      puts JSON.pretty_generate(payload)
+      BotBP::Manager.bot_telegram.send_update(payload)
 
       # Responde ao GitLab com um status 200 (OK)
       status 200
     end
+
   end
 end
